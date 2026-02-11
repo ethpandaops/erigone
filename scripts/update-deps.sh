bf2d9a9aa53fe2b13a133962669bbb2c78beae58
Matty Evans <mattevansnz@gmail.com>
2026-02-11 11:54:43 +1000

    chore(scripts): bump execution-processor to latest commit 8e71e1720ddf includes fixes for block-replay edge cases

--- scripts/update-deps.sh
+++ scripts/update-deps.sh
@@ -5,7 +5,7 @@ set -e
 
 echo "Adding erigone-specific dependencies..."
 
-go get github.com/ethpandaops/execution-processor@v0.1.6-0.20260206030723-55926964be58
+go get github.com/ethpandaops/execution-processor@v0.1.6-0.20260211014607-8e71e1720ddf
 go get github.com/creasty/defaults@v1.8.0
 go get github.com/redis/go-redis/v9@v9.17.2
 go get github.com/sirupsen/logrus@v1.9.3
